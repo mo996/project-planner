cf778c29cd8139e2174b9a6f4da8d47136fecd02
mo996 <mdaeeboul@gmail.com>
2024-07-15 14:41:36 +0200

    simple project planner

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddProjectView from '../views/AddProjectView.vue'
+import EditProjectView from '../views/EditProjectView.vue'
 
 const routes = [
   {
@@ -8,12 +10,15 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/add',
+    name: 'addProject',
+    component: AddProjectView
+  },
+  {
+    path: '/edit/:id',
+    name: 'editProject',
+    component: EditProjectView,
+    props: true
   }
 ]
 
